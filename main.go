9ca5739b85aacd80626276ee7b1a39e186afd4b5
Vignesh <vignesh@customd.com>
2025-12-26 06:51:09 +1300

    wip

---
+++ main.go
@@ -0,0 +1,39 @@
+//go:build linux
+// +build linux
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		fmt.Println("Unknown command")
+		return
+	}
+	fmt.Println("Hello, World!")
+}
+
+func run() {
+	fmt.Println("Running the application...", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
